af171d8b5db2ba551fa60df6749f92c7dafa6997
Sebastien Janaud <sebastien.janaud@gmail.com>
2015-05-26 18:23:14 +0200

    update catalog base

--- src/main/webapp/catalog/user-catalog-module.js
+++ src/main/webapp/catalog/user-catalog-module.js
@@ -134,7 +134,7 @@ JenSoft.Catalog = function(config){
 		//var catalogBaseContext = '/catalog/'+catalogGroup.replace('.','/')+'/'+catalogArtifact;
 		//var catalogBaseContext = catalogGroup.replace('.','/')+'/'+catalogArtifact;
 		//return catalogBaseContext;
-		return "/jensoft-io";
+		return "/";
 	}
 
 	/**
